ba8359ce8482a560c8537ad18a6548cf3caa1c21
Niccolo Cantu <niccolo.cantu@nina.no>
2025-05-02 17:04:49 +0200

    chg: read all file

--- src/dwca_parquet/templates/query.sql
+++ src/dwca_parquet/templates/query.sql
@@ -1,5 +1,5 @@
 select *, coalesce({% if 'footprintWKT' in columns %}ST_GeomFromText(footprintWKT), {% endif %}ST_POINT(decimalLongitude, decimalLatitude)) as geom
 from read_csv('{{core.path}}') as {{ core }}
 {% for ext in extensions -%}
-    join read_csv('{{ext.path}}') as {{ ext }} on {{ ext }}.{{ ext.id }}={{ core }}.{{ core.id }}
+    join read_csv('{{ext.path}}', sample_size=-1) as {{ ext }} on {{ ext }}.{{ ext.id }}={{ core }}.{{ core.id }}
 {% endfor %}
